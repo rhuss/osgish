a171a3bda03addc51c3276ca3bc61d67e8418c71
Roland Huß <roland@tichny.org>
2010-03-01 21:11:24 +0100

    added license text

--- agent/src/main/java/org/jmx4perl/osgish/OsgishActivator.java
+++ agent/src/main/java/org/jmx4perl/osgish/OsgishActivator.java
@@ -20,6 +20,30 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
 
+/*
+ * osgish - An OSGi Shell
+ *
+ * Copyright (C) 2009 Roland Huß, roland@cpan.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * A commercial license is available as well. Please contact roland@cpan.org for
+ * further details.
+ */
+
+
 /**
  * Activator for activation the embedded j4p agent as well
  * as the Aries JMX bundle. So it's an aggregat activator.
--- agent/src/main/java/org/jmx4perl/osgish/OsgishService.java
+++ agent/src/main/java/org/jmx4perl/osgish/OsgishService.java
@@ -9,6 +9,30 @@ import javax.management.MBeanServer;
 import javax.management.MalformedObjectNameException;
 import javax.management.ObjectName;
 
+/*
+ * osgish - An OSGi Shell
+ *
+ * Copyright (C) 2009 Roland Huß, roland@cpan.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * A commercial license is available as well. Please contact roland@cpan.org for
+ * further details.
+ */
+
+
 /**
  * Implementation of a service layer for communication between 'osgish' and the
  * osgi agent bundle.
